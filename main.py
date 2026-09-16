9f9b074297603c56a822ca87b28b306f59a8777c
Robbie <rdlee.0718@gmail.com>
2026-09-16 12:11:30 -0500

    Changed the response if the answer cannot be found in context given

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def ask_rag(question: str):
     context = retrieve_context(question)
     
     system_prompt = f"""You are a helpful assistant. Answer the question using ONLY the provided context.
-If the answer isn't in the context, state that you don't know.
+If the answer isn't in the context, state that it is not in the database.
 
 Context:
 {context}"""
