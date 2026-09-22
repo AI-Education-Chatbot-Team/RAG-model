53fbcd9855779f1cc91845eb3db8dd6b92e7c296
Robbie <rdlee.0718@gmail.com>
2026-09-22 13:11:04 -0500

    Adjusted groq model to use a less powerful model with less temp

--- main.py
+++ main.py
@@ -37,12 +37,12 @@ Context:
 {context}"""
 
     completion = client.chat.completions.create(
-        model="openai/gpt-oss-120b",
+        model="openai/gpt-oss-20b",
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": question}
         ],
-        temperature=1,
+        temperature=0.3,
         max_completion_tokens=2048,
         top_p=1,
         reasoning_effort="medium",
